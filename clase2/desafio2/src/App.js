dd3f2fe3187a0dbe1dc922ed7bf63e398612f105
Bruno de Renzis <brunoderenzisrubinetti26@gmail.com>
2022-05-31 13:07:49 -0300

    Desafío 2

---
+++ clase2/desafio2/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/navbar.jsx';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <NavBar/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ clase2/desafio2/src/components/navbar.jsx
@@ -0,0 +1,16 @@
+const NavBar = ()=>{
+    return(
+        <div className="menu-ecommerce">
+            <nav>
+                <ul>
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="#">Nosotros</a></li>
+                    <li><a href="#">Contacto</a></li>
+                    <li><a href="#">Ayuda</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar;
