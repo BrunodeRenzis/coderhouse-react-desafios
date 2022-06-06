41bb755553bd1f6ed93ecb98bb732215dc4898e1
Bruno de Renzis <brunoderenzisrubinetti26@gmail.com>
2022-06-05 22:35:57 -0300

    Ajustes E-Commerce

--- clase3/clase3-componentes/src/App.jsx
+++ clase3/clase3-componentes/src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar.jsx'
 import { ItemListContainer } from './components/ItemListContainer/ItemListContainer';
+import { FooterApp } from './components/FooterApp/FooterApp';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <NavBar></NavBar>
       </header>
-      <ItemListContainer greeting="Hola! bienvenido"/>
+      <ItemListContainer greeting="Hola Bienvenido!"/>
+      <FooterApp/>
     </div>
   );
 }
