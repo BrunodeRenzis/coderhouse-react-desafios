26e90f972fac3c730ef36f290f7895beec3f2ec8
Bruno de Renzis <brunoderenzisrubinetti26@gmail.com>
2022-06-20 19:13:02 -0300

    Subo desafio clase 6 y modificaciones clase 5

---
+++ clase6/item-detail/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <ItemDetailContainer/>
+    </div>
+  );
+}
+
+export default App;
